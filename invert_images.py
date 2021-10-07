354652c68587e7c59ba7ef9326bda4bda4cbe9ed
benmulcahy406 <58439480+benmulcahy406@users.noreply.github.com>
2021-10-07 09:05:40 -0400

    Update invert_images.py

--- invert_images.py
+++ invert_images.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
+@author: Ben Mulcahy
 
-This is a temporary script file.
+Inverts all images inside a specified directory
 """
 import os
 from PIL import Image
 import PIL.ImageOps    
 
-path = 'C:/Users/mulcahy/Desktop/dauer_mEMbrain/GT3_2021_07_19/temp/'
+path = 'C:/Users/mulcahy/Desktop/dauer_mEMbrain/GT3_2021_07_19/temp/' # change as necessary
 
+# loop through all images in path, invert them and save them back to original location (overwrite)
 for f in os.listdir(path):
     fpath = path + f
     image = Image.open(fpath)
